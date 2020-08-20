7d1935661ff8e8bcc4402b38c45c31a2f1913edb
kadvar <kadvar@yahoo.com>
2020-08-20 12:06:18 -0700

    automated service catalog account creation

---
+++ create_account.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+PROVISIONED_PRODUCT_NAME="dev-acct-"$(date | md5 | head -c5)
+PROVISION_TOKEN=$(date | md5)
+PRODUCT_ID="prod-n2mfbfheqlkzm"
+PROVISIONING_ARTIFACT_ID="pa-pyewfptayx4im"
+PARAM_FILE="file://params.json"
+
+echo "Creating Account Factory account: "$PROVISIONED_PRODUCT_NAME
+
+aws servicecatalog provision-product --product-id $PRODUCT_ID --provisioning-artifact-id $PROVISIONING_ARTIFACT_ID --provision-token $PROVISION_TOKEN --provisioned-product-name $PROVISIONED_PRODUCT_NAME --provisioning-parameters file://params.json
+
